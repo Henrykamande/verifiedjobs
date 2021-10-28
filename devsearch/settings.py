1e96c4c33b3be0ddb4b6583978b44b882a00e152
henrykamande1990@gmail.com <henrykamande1990@gmail.com>
2021-10-28 15:42:51 +0300

    static root setup

--- devsearch/settings.py
+++ devsearch/settings.py
@@ -197,7 +197,7 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS =[
     os.path.join(BASE_DIR, 'static')
 ]
-STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static_root')
 MEDIA_URL='/media/agency_uploads/'
 
 MEDIA_ROOT= os.path.join(BASE_DIR, 'static/images')
